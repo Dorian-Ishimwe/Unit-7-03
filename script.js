45525f0d4fbe194693ce4c2e4ba45a992367c037
DorianI <>
2021-05-08 14:13:11 +0000

    fixed indents

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ let userInput = 0
 document.getElementById('button').addEventListener('click', age)
 
 function age () {
- userInput = document.getElementById('age').value
- userInput = parseInt(userInput)
+  userInput = document.getElementById('age').value
+  userInput = parseInt(userInput)
 
- if (userInput >= 18) {
-   document.getElementById('answer').innerHTML = 'You can watch R rated movies'
+  if (userInput >= 18) {
+    document.getElementById('answer').innerHTML = 'You can watch R rated movies'
   } else if (userInput >= 13) {
-   document.getElementById('answer').innerHTML = 'You can watch PG-13 rated movies'
+    document.getElementById('answer').innerHTML = 'You can watch PG-13 rated movies'
   } else if (userInput < 13) {
     document.getElementById('answer').innerHTML = 'You can only watch PG and G rated movies'
   }
